19c1a21ec6e83ce763954c319e8ac7b0886bb9eb
ali.teeney <alistair.teeney@octoenergy.com>
2020-02-03 15:45:07 +1100

    Let version regex match development tags in test

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,5 +5,27 @@ from timeserio.version import get_git_version
 
 def test_version():
     ver = get_git_version(abbrev=7)
-    VER_RE = r"^(\d+.\d+.\d+)(-\d+)*(-g\S{7})?(-dirty)?$"
+    # Versions should comply with semver, which is where this regex is from
+    # see semver.org and github.com/python-semver/python-semver
+    VER_RE = re.compile(
+        r"""
+            ^
+            (?P<major>0|[1-9]\d*)
+            \.
+            (?P<minor>0|[1-9]\d*)
+            \.
+            (?P<patch>0|[1-9]\d*)
+            (?:-(?P<prerelease>
+                (?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)
+                (?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*
+            ))?
+            (?:\+(?P<build>
+                [0-9a-zA-Z-]+
+                (?:\.[0-9a-zA-Z-]+)*
+            ))?
+            $
+            """,
+        re.VERBOSE,
+    )
+
     assert re.match(VER_RE, ver)
