2d3558299fdca61dd3e40c243c33cdb2c2098485
Igor <igor.gotlibovych@gmail.com>
2021-03-10 14:36:27 +0000

    Hard-code __version__. NB: consider poetry+versioneer or similar
    Before this change, the version was single-sourced from git tags.
    After this change, the version is hard-coded in two places:
    - pyproject.toml
    - version.py
    
    An automated solution should be considered

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,31 +1,5 @@
-import re
-
-from timeserio.version import get_git_version
+from timeserio.version import __version__
 
 
 def test_version():
-    ver = get_git_version(abbrev=7)
-    # Versions should comply with semver, which is where this regex is from
-    # see semver.org and github.com/python-semver/python-semver
-    VER_RE = re.compile(
-        r"""
-            ^
-            (?P<major>0|[1-9]\d*)
-            \.
-            (?P<minor>0|[1-9]\d*)
-            \.
-            (?P<patch>0|[1-9]\d*)
-            (?:-(?P<prerelease>
-                (?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)
-                (?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*
-            ))?
-            (?:\+(?P<build>
-                [0-9a-zA-Z-]+
-                (?:\.[0-9a-zA-Z-]+)*
-            ))?
-            $
-            """,
-        re.VERBOSE,
-    )
-
-    assert re.match(VER_RE, ver)
+    assert len(__version__.split(".")) == 3
