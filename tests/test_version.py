7fc9fd1b1bd46b4362b4b989491def189aee3a0c
ali-tny <aliteeney@googlemail.com>
2022-03-24 14:30:32 +0000

    Move version to be PEP 440 compliant
    This is to support poetry: currently we're using SemVer, but installing
    dev versions via poetry gives a build error, then says it's installed it
    but never actually does. The PyPI released versions will be both SemVer
    _and_ PEP 440 compliant. Potentially if we move some other projects away
    from poetry then we could change this back.
    
    The version can only have a major, minor and patch, an optional dev
    version and a "local" version after a +.
    
    We move the number of commits after a tag to the dev version, and the
    hash & dirty flag to the local version.

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,26 +5,40 @@ from timeserio.version import get_git_version
 
 def test_version():
     ver = get_git_version(abbrev=7)
-    # Versions should comply with semver, which is where this regex is from
-    # see semver.org and github.com/python-semver/python-semver
+    # Versions should comply with PEP440, which is where this regex is from
+    # see https://peps.python.org/pep-0440/ and regex here:
+    # https://github.com/pypa/packaging/blob/b5f0efdf3986725c2a45437e03d0ee5939c21192/packaging/version.py#L159 # noqa
     VER_RE = re.compile(
         r"""
-            ^
-            (?P<major>0|[1-9]\d*)
-            \.
-            (?P<minor>0|[1-9]\d*)
-            \.
-            (?P<patch>0|[1-9]\d*)
-            (?:-(?P<prerelease>
-                (?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)
-                (?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*
-            ))?
-            (?:\+(?P<build>
-                [0-9a-zA-Z-]+
-                (?:\.[0-9a-zA-Z-]+)*
-            ))?
-            $
-            """,
+        v?
+        (?:
+            (?:(?P<epoch>[0-9]+)!)?                           # epoch
+            (?P<release>[0-9]+(?:\.[0-9]+)*)                  # release segment
+            (?P<pre>                                          # pre-release
+                [-_\.]?
+                (?P<pre_l>(a|b|c|rc|alpha|beta|pre|preview))
+                [-_\.]?
+                (?P<pre_n>[0-9]+)?
+            )?
+            (?P<post>                                         # post release
+                (?:-(?P<post_n1>[0-9]+))
+                |
+                (?:
+                    [-_\.]?
+                    (?P<post_l>post|rev|r)
+                    [-_\.]?
+                    (?P<post_n2>[0-9]+)?
+                )
+            )?
+            (?P<dev>                                          # dev release
+                [-_\.]?
+                (?P<dev_l>dev)
+                [-_\.]?
+                (?P<dev_n>[0-9]+)?
+            )?
+        )
+        (?:\+(?P<local>[a-z0-9]+(?:[-_\.][a-z0-9]+)*))?       # local version
+        """,
         re.VERBOSE,
     )
 
